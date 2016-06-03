460705bd3bf6d88cb350bb39a200862d4dd2f0d9
Woody <wwoody827@gmail.com>
2016-06-02 22:34:03 -0400

    add

--- App/CutHallBar.py
+++ App/CutHallBar.py
@@ -11,16 +11,16 @@ import numpy as np
 
 
 # Create object corresponding to the motor.83840805
-MotorX = APTMotor(83829690, HWTYPE=31) # The number should correspond to the serial number.
-MotorY = APTMotor(83840805, HWTYPE=31) # The number should correspond to the serial number.
+# MotorX = APTMotor(83829690, HWTYPE=31) # The number should correspond to the serial number.
+# MotorY = APTMotor(83840805, HWTYPE=31) # The number should correspond to the serial number.
 # Use help APTMotor to obtain full list of hardware (HW) supported.
 
 # Homing Two Motors
-print("Homing X")
-MotorX.go_home()
-print("Homing Y")
-MotorY.go_home()
-# print("Homing End")
+#print("Homing X")
+#MotorX.go_home()
+#print("Homing Y")
+#MotorY.go_home()
+#print("Homing End")
 
 # 
 # MotorX.mAbs(10.0)
@@ -33,25 +33,32 @@ with open('sixterminal 250um.txt') as f:
     for line in f:
         line = line.split() # to deal with blank 
         if line:            # lines (ie skip them)
-    r        line = [float(i) for i in line]
+            line = [float(i) for i in line]
             # line = line[0,1]
             # print(line)
             polyShapeList.append(line)
           
 polyShape = np.asarray(polyShapeList)
 plt.plot(polyShape[:, 0], polyShape[:,1])
-plt.draw()
+# plt.draw()
+# plt.show()
 
-
-
-plt.show()
+platform = TwoAxisPlatform()
+print(platform.getCurrentPos())
     
 class TwoAxisPlatform:
     
-    def __init__(self):
-        self.positionX = 0
-        self.positionY = 0
-        self.stepSize = 0.1
+    def __init__(self, max = [25, 25], stepSize = 0.1):
+        self.position = [0, 0]
+        self.max = max
+        self.stepSize = stepSize
         
-    def moveTo(self, next):
+    def resetPos(self, position = [0, 0]):
+        self.position = position
         
+    def getCurrentPosX(self):
+        return self.position
+    
+
+    def moveTo(self, nextX, nextY):
+        return true
